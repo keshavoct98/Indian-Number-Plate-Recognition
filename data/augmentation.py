6a8e52556dbe72a82d16daa201a9aa7dbb3b959e
keshav sharma <keshavoct98@gmail.com>
2020-06-15 21:28:32 +0530

    Add files via upload

---
+++ data/augmentation.py
@@ -0,0 +1,53 @@
+# Importing libraries
+import cv2
+import numpy as np
+import pandas as pd
+from augmentation_methods import draw_box, pad, shear
+
+# Reading license plate coordinates csv file
+df = pd.read_csv('dataset/indian_license_plates.csv')
+
+def augmentAndSave(df_row, aug_type):
+    img = cv2.imread('dataset/Indian Number Plates/'+df_row[0]+'.jpeg')
+    bbox = np.array((df_row[3]*df_row[1], df_row[4]*df_row[2], df_row[5]*df_row[1], df_row[6]*df_row[2]))
+    
+    if aug_type == 'pad':
+        img_new, new_bbox = pad(img, bbox.copy(), 16)  
+    elif aug_type == 'shearYNeg':
+        img_new, new_bbox = shear(img, bbox.copy(), -0.1, 1)
+    elif aug_type == 'shearYPos':
+        img_new, new_bbox = shear(img, bbox.copy(), 0.1, 1)
+    elif aug_type == 'shearXNeg':
+        img_new, new_bbox = shear(img, bbox.copy(), -0.1, 0)
+    elif aug_type == 'shearXPos':
+        img_new, new_bbox = shear(img, bbox.copy(), 0.1, 0)
+    cv2.imwrite('dataset/Indian Number Plates/'+aug_type+'_'+df.iloc[i,0]+'.jpeg', img_new)
+    
+    dict_for_df = {'image_name':aug_type+'_'+df_row[0], 'image_width':img_new.shape[1], 
+                   'image_height':img_new.shape[0], 'top_x':new_bbox[0]/img_new.shape[1], 
+                   'top_y':new_bbox[1]/img_new.shape[0], 'bottom_x':new_bbox[2]/img_new.shape[1],
+                   'bottom_y':new_bbox[3]/img_new.shape[0]}
+    
+    return dict_for_df
+
+No_of_images = df.shape[0]
+# Padding images
+for i in range(0, No_of_images):
+    if i%10 == 0: # Images skipped for vaildation set
+        continue
+    else:
+        df = df.append(augmentAndSave(df.iloc[i, :], 'pad'), ignore_index=True)
+
+No_of_images = 2*(No_of_images - (int(No_of_images/10) + 1)) + (int(No_of_images/10) + 1)
+# Shearing images
+for i in range(0, No_of_images):
+    if i%10 == 0 and i<236:
+        continue
+    else:
+        df = df.append(augmentAndSave(df.iloc[i, :], 'shearYNeg'), ignore_index=True)
+        df = df.append(augmentAndSave(df.iloc[i, :], 'shearYPos'), ignore_index=True)
+        df = df.append(augmentAndSave(df.iloc[i, :], 'shearXNeg'), ignore_index=True)
+        df = df.append(augmentAndSave(df.iloc[i, :], 'shearXPos'), ignore_index=True)
+
+df = df.sample(frac=1).reset_index(drop=True)
+df.to_csv('dataset/indian_license_plates.csv', index = None)
---
+++ data/augmentation_methods.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import img_as_ubyte
+from skimage import transform as tf
+
+def draw_box(img, crd): # draw bounding box
+    
+    img_copy = img.copy()
+    cv2.rectangle(img_copy, (int(crd[0]), int(crd[1])), (int(crd[2]), int(crd[3])), (0,0,255), 3)
+    plt.imshow(cv2.cvtColor(img_copy, cv2.COLOR_BGR2RGB))
+    plt.show()
+    
+def pad(img, crd, resize_val): # resize image and calculates new bounding box coordinates
+    
+    pad_y, pad_x = int(img.shape[0]/resize_val), int(img.shape[1]/resize_val)
+    img_padded = cv2.copyMakeBorder(img,pad_y,pad_y,pad_x,pad_x,cv2.BORDER_CONSTANT)
+    img_padded = cv2.resize(img_padded, (img.shape[1], img.shape[0]))
+    
+    crd[0],crd[1],crd[2],crd[3] = crd[0]+pad_x,crd[1]+pad_y,crd[2]+pad_x,crd[3]+pad_y
+    new_crd = (resize_val/(resize_val+2)) * crd
+    
+    return img_padded, new_crd
+
+def shear(img, crd, shear_val, dir_xy): # shear image  and calculates new bounding box coordinates
+    
+    if dir_xy == 0:
+        arr = np.array([[1,shear_val,0], [0,1,0], [0,0,1]])
+    else:
+        arr = np.array([[1,0,0], [shear_val,1,0], [0,0,1]])
+        
+    afine_tf = tf.AffineTransform(matrix = arr)
+    img_sheared = tf.warp(img, inverse_map=afine_tf)
+    img_sheared = img_as_ubyte(img_sheared)
+    
+    if dir_xy == 0:
+        crd[0] = crd[0] - shear_val * crd[1]
+        crd[2] = crd[2] - shear_val * crd[3]
+        if crd[0] < 0:
+            crd[0] = 0
+        if crd[2] >= img.shape[1]:
+            crd[2] = img.shape[1]-1
+    else:
+        crd[1] = crd[1] - shear_val * crd[0]
+        crd[3] = crd[3] - shear_val * crd[2]
+        if crd[1] < 0:
+            crd[1] = 0
+        if crd[3] >= img.shape[0]:
+            crd[3] = img.shape[0]-1
+
+    return img_sheared, crd
